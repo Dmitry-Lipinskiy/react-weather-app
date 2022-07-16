09cc8b7e1d811109cdae3901e9b8d2c31d29a365
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2022-07-16 19:48:23 +0300

    add http, made a request for the weather today

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import http from '../../http';
 import './header.css'
 
+const API_KEY = "1c99c168e5f32cae7dc01015c0038525";
+let lang: string = "en";
+let city: any = "";
+
 const Header = () => {
+
+  const [value, setValue] = useState<any>(city);
+
+  const getWeatherToday = (city: any) => {
+    http.get(`weather?q=${city}&limit=1&appid=${API_KEY}&units=metric&lang=${lang}`).then(res => {
+      setValue("");
+      console.log(res);
+    }).catch(err => console.log(err));
+  }
 
   return (
     <div className='header-container'>
       <h2>MY WEATHER</h2>
       <div className='header-options-block'>
-        <input type="text" className="form-control" placeholder="City name"/>
-        <button type="button" className="btn btn-success">Search</button>
+        <input type="text" 
+          className="form-control" 
+          placeholder="City name"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button type="button" 
+          className="btn btn-success" 
+          onClick={() => getWeatherToday(value)}
+        >Search</button>
         <button type="button" className="btn btn-primary">Sing in</button>
       </div>
     </div>
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
       <div className="container-fluid">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to='today' className="nav-link active" aria-current="page">Today</Link>
+            <Link to='today' className="nav-link" aria-current="page">Today</Link>
           </li>
           <li className="nav-item">
             <Link to='fiveDays' className="nav-link">5-day forecast</Link>
